9a386001d802eca9075253c868696dac5b67ef22
Philip Withnall <philip@tecnocode.co.uk>
2010-01-03 13:17:30 +0000

    [build] Remove intltool scripts, introspection.m4, and rename COPYING.LIB
    Remove mention of the intltool scripts from the Makefile, since
    intltool >= 0.40.0 doesn't require them to be in-tree. Rename COPYING.LIB to
    COPYING to be more standardised, and remove introspection.m4 from the tree
    since it too can be installed system-wide.

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,7 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="gdata"
+REQUIRED_M4MACROS=introspection.m4
 
 (test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
