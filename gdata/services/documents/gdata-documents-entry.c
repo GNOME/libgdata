782a7a1cbd613de08b47d639f3d72eb678cb9bd0
Philip Withnall <philip@tecnocode.co.uk>
2010-12-11 19:13:22 +0000

    documents: Actually store the content-type notification signal handler ID
    This is the third commit to fix this trivial problem. I need more sugar.

--- gdata/services/documents/gdata-documents-entry.c
+++ gdata/services/documents/gdata-documents-entry.c
@@ -646,7 +646,7 @@ _gdata_documents_entry_download_document (GDataDocumentsEntry *self, GDataServic
 	/* Synchronously splice the data from the download stream to the file stream (network -> disk) */
 	src_stream = gdata_download_stream_new (GDATA_SERVICE (service), src_uri);
 	if (content_type != NULL)
-		g_signal_connect (src_stream, "notify::content-type", (GCallback) notify_content_type_cb, content_type);
+		content_type_signal = g_signal_connect (src_stream, "notify::content-type", (GCallback) notify_content_type_cb, content_type);
 
 	g_output_stream_splice (G_OUTPUT_STREAM (dest_stream), src_stream,
 				G_OUTPUT_STREAM_SPLICE_CLOSE_SOURCE | G_OUTPUT_STREAM_SPLICE_CLOSE_TARGET, cancellable, &child_error);
