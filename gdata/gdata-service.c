db6ee229558fcd749fc2e6ea2b2fb26bfa46d082
Philip Withnall <philip@tecnocode.co.uk>
2011-08-20 17:38:20 +0100

    core: Fix a NULL pointer ref. inc. in GDataService

--- gdata/gdata-service.c
+++ gdata/gdata-service.c
@@ -285,9 +285,12 @@ real_append_query_headers (GDataService *self, GDataAuthorizationDomain *domain,
 	if (self->priv->authorizer != NULL) {
 		gdata_authorizer_process_request (self->priv->authorizer, domain, message);
 
-		/* Store the authorisation domain on the message so that we can access it again after refreshing authorisation if necessary.
-		 * See _gdata_service_send_message(). */
-		g_object_set_data_full (G_OBJECT (message), "gdata-authorization-domain", g_object_ref (domain), (GDestroyNotify) g_object_unref);
+		if (domain != NULL) {
+			/* Store the authorisation domain on the message so that we can access it again after refreshing authorisation if necessary.
+			 * See _gdata_service_send_message(). */
+			g_object_set_data_full (G_OBJECT (message), "gdata-authorization-domain", g_object_ref (domain),
+			                        (GDestroyNotify) g_object_unref);
+		}
 	}
 
 	/* Set the GData-Version header to tell it we want to use the v2 API */
