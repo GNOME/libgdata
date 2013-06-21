59aea63e87277fe7d320add954403105be3f0405
Peteris Krisjanis <pecisk@gmail.com>
2013-06-21 18:35:45 +0300

    Change g_log_set_handler to g_log_set_default_handler so we could set handler for debug log level in tests/common.c.

--- gdata/gdata-service.c
+++ gdata/gdata-service.c
@@ -195,7 +195,7 @@ gdata_service_init (GDataService *self)
 	self->priv->session = _gdata_service_build_session ();
 
 	/* Debug log handling */
-	g_log_set_handler (G_LOG_DOMAIN, G_LOG_LEVEL_DEBUG, (GLogFunc) debug_handler, self);
+	g_log_set_default_handler ((GLogFunc) debug_handler, self);
 
 	/* Proxy the SoupSession's proxy-uri and timeout properties */
 	g_signal_connect (self->priv->session, "notify::proxy-uri", (GCallback) notify_proxy_uri_cb, self);
