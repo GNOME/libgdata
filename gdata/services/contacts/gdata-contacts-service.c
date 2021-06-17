a29b4fc75ad65ddb42e0f3eca2691bc49d5a1fc1
Philip Withnall <pwithnall@endlessos.org>
2021-06-17 13:26:03 +0100

    contacts: Fix deprecation version for Contacts service
    0.18.0 has already been released; the deprecation will land in 0.18.2.
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

--- gdata/services/contacts/gdata-contacts-service.c
+++ gdata/services/contacts/gdata-contacts-service.c
@@ -149,7 +149,7 @@
  * </example>
  *
  * Since: 0.2.0
- * Deprecated: 0.18.0: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
+ * Deprecated: 0.18.2: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
  */
 
 #include <config.h>
@@ -201,7 +201,7 @@ get_authorization_domains (void)
  * Return value: a new #GDataContactsService, or %NULL; unref with g_object_unref()
  *
  * Since: 0.9.0
- * Deprecated: 0.18.0: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
+ * Deprecated: 0.18.2: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
  */
 GDataContactsService *
 gdata_contacts_service_new (GDataAuthorizer *authorizer)
@@ -225,7 +225,7 @@ gdata_contacts_service_new (GDataAuthorizer *authorizer)
  * Return value: (transfer none): the service's authorization domain
  *
  * Since: 0.9.0
- * Deprecated: 0.18.0: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
+ * Deprecated: 0.18.2: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
  */
 GDataAuthorizationDomain *
 gdata_contacts_service_get_primary_authorization_domain (void)
@@ -249,7 +249,7 @@ gdata_contacts_service_get_primary_authorization_domain (void)
  * Return value: (transfer full): a #GDataFeed of query results; unref with g_object_unref()
  *
  * Since: 0.2.0
- * Deprecated: 0.18.0: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
+ * Deprecated: 0.18.2: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
  */
 GDataFeed *
 gdata_contacts_service_query_contacts (GDataContactsService *self, GDataQuery *query, GCancellable *cancellable,
@@ -298,7 +298,7 @@ gdata_contacts_service_query_contacts (GDataContactsService *self, GDataQuery *q
  * and gdata_service_query_async(), which is the base asynchronous query function.
  *
  * Since: 0.9.1
- * Deprecated: 0.18.0: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
+ * Deprecated: 0.18.2: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
  */
 void
 gdata_contacts_service_query_contacts_async (GDataContactsService *self, GDataQuery *query, GCancellable *cancellable,
@@ -347,7 +347,7 @@ gdata_contacts_service_query_contacts_async (GDataContactsService *self, GDataQu
  * Return value: (transfer full): an updated #GDataContactsContact, or %NULL; unref with g_object_unref()
  *
  * Since: 0.2.0
- * Deprecated: 0.18.0: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
+ * Deprecated: 0.18.2: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
  */
 GDataContactsContact *
 gdata_contacts_service_insert_contact (GDataContactsService *self, GDataContactsContact *contact, GCancellable *cancellable, GError **error)
@@ -386,7 +386,7 @@ gdata_contacts_service_insert_contact (GDataContactsService *self, GDataContacts
  * and gdata_service_insert_entry_async(), which is the base asynchronous insertion function.
  *
  * Since: 0.7.0
- * Deprecated: 0.18.0: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
+ * Deprecated: 0.18.2: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
  */
 void
 gdata_contacts_service_insert_contact_async (GDataContactsService *self, GDataContactsContact *contact, GCancellable *cancellable,
@@ -420,7 +420,7 @@ gdata_contacts_service_insert_contact_async (GDataContactsService *self, GDataCo
  * Return value: (transfer full): a #GDataFeed of query results; unref with g_object_unref()
  *
  * Since: 0.7.0
- * Deprecated: 0.18.0: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
+ * Deprecated: 0.18.2: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
  */
 GDataFeed *
 gdata_contacts_service_query_groups (GDataContactsService *self, GDataQuery *query, GCancellable *cancellable,
@@ -469,7 +469,7 @@ gdata_contacts_service_query_groups (GDataContactsService *self, GDataQuery *que
  * which is the base asynchronous query function.
  *
  * Since: 0.9.1
- * Deprecated: 0.18.0: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
+ * Deprecated: 0.18.2: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
  */
 void
 gdata_contacts_service_query_groups_async (GDataContactsService *self, GDataQuery *query, GCancellable *cancellable,
@@ -516,7 +516,7 @@ gdata_contacts_service_query_groups_async (GDataContactsService *self, GDataQuer
  * Return value: (transfer full): the inserted #GDataContactsGroup; unref with g_object_unref()
  *
  * Since: 0.7.0
- * Deprecated: 0.18.0: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
+ * Deprecated: 0.18.2: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
  */
 GDataContactsGroup *
 gdata_contacts_service_insert_group (GDataContactsService *self, GDataContactsGroup *group, GCancellable *cancellable, GError **error)
@@ -568,7 +568,7 @@ gdata_contacts_service_insert_group (GDataContactsService *self, GDataContactsGr
  * gdata_service_insert_entry_async(), which is the base asynchronous insertion function.
  *
  * Since: 0.7.0
- * Deprecated: 0.18.0: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
+ * Deprecated: 0.18.2: The Google Contacts service is deprecated. Use Google’s CardDAV interface instead.
  */
 void
 gdata_contacts_service_insert_group_async (GDataContactsService *self, GDataContactsGroup *group, GCancellable *cancellable,
